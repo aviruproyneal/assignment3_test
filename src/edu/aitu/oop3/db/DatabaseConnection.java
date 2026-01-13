5e746c97275cdf5157e7db44cfbf26beb1e1e607
Avirup Neal <learnneal@gmail.com>
2026-01-13 09:07:28 +0500

    Second commit

--- src/edu/aitu/oop3/db/DatabaseConnection.java
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -1,17 +1,31 @@
 package edu.altu.oop3.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-
     private static final String URL =
             "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
-
     private static final String USER = "postgres.slgskrauppciihcotnds";
+    private static final String PASSWORD = loadPassword(); // ← Changed!
 
-    private static final String PASSWORD = "Uhm@Uhcc843u9et";
+    private static String loadPassword() {
+        Properties props = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            props.load(input);
+            String password = props.getProperty("DB_PASSWORD");
+            if (password == null || password.isBlank()) {
+                throw new RuntimeException("DB_PASSWORD is not set in config.properties");
+            }
+            return password;
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot load DB_PASSWORD from config.properties", e);
+        }
+    }
 
     private DatabaseConnection() {}
 
